ab69bdab6ddceaea3920960b7c0ba96a762a2539
Hamed Sahraei <Hamed.Sahraei@student.vives.be>
2026-03-03 15:56:43 +0100

    ok

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ from functions import is_even
 import requests
 response = requests.get("https://api.chucknorris.io/jokes/random")
 print(f"statuscode: {response.status_code}")
-print(response.text)
+print(responsetext)
